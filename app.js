a8029853a0739041b7c51e6ce3b09a381e190320
Alex Cherubin <agcpbg@yahoo.com>
2017-03-06 17:34:24 -0600

    nunjucks setup complete

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+
+// res.render( 'index', {title: 'Hall of Fame', people: people} );
 
 
 app.use(morgan('dev'));
 
 app.get('/', function(req, res) {
 	console.log("Welcome nodemon!");
-	res.send("welcome sent");
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 
